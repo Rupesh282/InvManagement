2a115d4a4f36ca7416aa7b393fa1da0889295f42
Rupesh Kalantre <rupu.kalantre@gmail.com>
2020-11-28 22:54:53 +0530

    added table function

--- pgsql_login_details/pg_client.php
+++ pgsql_login_details/pg_client.php
@@ -46,6 +46,25 @@ class pg_client {
         return pg_affected_rows($result);
     }
 
+    function build_table($array) {
+        // start table
+        $html = '<table>';
+        // header row
+        $html .= '<tr>';
+        foreach ($array[0] as $key => $value) {
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+        $html .= '</tr>';
+
+        // data rows
+        foreach ($array as $key => $value) {
+            $html .= '<tr>';
+            foreach ($value as $key2 => $value2) {
+                $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+            }
+            $html .= '</tr>';
+        }
+    }
 }
 
 $pg_username = $data_base_config["pg_username"];
